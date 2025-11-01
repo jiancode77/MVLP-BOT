f3d7d0c01d501bfc8aa6e01739b9b7b99de41fbf
Jian Unknown <bintangkecilku3374@gmail.com>
2025-11-01 13:34:54 +0700

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -1,5 +1,5 @@
 export const name = 'info';
 export async function execute({ sock, sender, pushname }) {
-  const message = `Halo ${pushname}!\nBot WhatsApp aktif dan berjalan dengan baik.`;
+  const message = `👋 Halo ${pushname}!\n\n🤖 Bot WhatsApp aktif dan berjalan dengan baik.\n📍 Gunakan prefix: !\n📂 Total plugins: 3`;
   await sock.sendMessage(sender, { text: message });
 }
